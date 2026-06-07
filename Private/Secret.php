7ad14e686f77bf8081b6ea421c7ac8d6e15ba7d3
wealthmetre-crypto <wealthmetre@gmail.com>
2026-06-07 12:50:39 +0000

    security: remove sensitive files from tracking

--- Private/Secret.php
+++
@@ -1,3 +0,0 @@
-<?php
-// file permissions 400/440 रखें
-const WM_OPENAI_KEY = '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';
